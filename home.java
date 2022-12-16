703b08b3a80a37bcf84259c3e327be602e664af9
Pragatinimbargi <pragatirc100@gmail.com>
2022-12-16 15:42:58 +0530

    first code

---
+++ home.java
@@ -0,0 +1,12 @@
+class home
+{
+public static void main(String[] args)
+{
+String hname="devalaya";
+double coast=1350000;
+String color="PINK AND CREAM";
+System.out.println(hname);
+System.out.println(coast);
+System.out.println(color);
+}
+}
