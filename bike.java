e55a038ed483d5b30377234a125c45fc4cbfcc82
Pragatinimbargi <pragatirc100@gmail.com>
2022-12-16 15:22:30 +0530

    second code

---
+++ bike.java
@@ -0,0 +1,12 @@
+class bike
+{
+public static void main(String[] args)
+{
+int coast=50000;
+String bname="dreamyuga";
+String color="balck";
+System.out.println( coast);
+System.out.println(bname);
+System.out.println(color);
+}
+}
